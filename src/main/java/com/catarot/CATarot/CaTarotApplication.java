ea5bd8c2f7c7f0b586967dba8423abfc02fcdfcc
ElizabethLeeWH <leewenhui98@gmail.com>
2023-11-22 01:21:52 +0800

    first commit

---
+++ src/main/java/com/catarot/CATarot/CaTarotApplication.java
@@ -0,0 +1,13 @@
+package com.catarot.CATarot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaTarotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaTarotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catarot/CATarot/CaTarotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catarot.CATarot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaTarotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
